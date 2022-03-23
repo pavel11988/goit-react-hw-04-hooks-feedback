79deff63449f25fb483f172041ec372a2fe4d843
pavel11988 <kossoua88@gmail.com>
2022-03-23 02:15:41 +0200

    delete trash

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,64 +8,6 @@ import { Container } from './App.styled';
 import Section from './Section/Section';
 import { useState, useEffect } from 'react';
 
-// class App extends React.Component {
-//   static defaultProps = {
-//     initialValue: 0,
-//   };
-
-//   state = {
-//     good: this.props.initialValue,
-//     neutral: this.props.initialValue,
-//     bad: this.props.initialValue,
-//   };
-
-//   handleIncrement = name => {
-//     this.setState(prevState => {
-//       return {
-//         ...prevState,
-//         [name]: prevState[name] + 1,
-//       };
-//     });
-//   };
-
-//   countTotalFeedback = () => {
-//     return this.state.good + this.state.bad + this.state.neutral;
-//   };
-
-//   countPositiveFeedbackPercentage = total => {
-//     return Math.round((this.state.good / total) * 100);
-//   };
-
-//   render() {
-//     const options = ['good', 'neutral', 'bad'];
-//     const total = this.countTotalFeedback();
-//     const positiveFeedback = this.countPositiveFeedbackPercentage(total);
-//     return (
-//       <Container>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={options}
-//             onLeaveFeedback={this.handleIncrement}
-//           />
-//         </Section>
-
-//         <Section title="Statistic">
-//           {total > 0 ? (
-//             <Statistic
-//               good={this.state.good}
-//               neutral={this.state.neutral}
-//               bad={this.state.bad}
-//               total={total}
-//               positivePercentage={positiveFeedback}
-//             />
-//           ) : (
-//             <Notification message="There is no feedback" />
-//           )}
-//         </Section>
-//       </Container>
-//     );
-//   }
-// }
 export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
