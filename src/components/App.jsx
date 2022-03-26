b77dc71c26f5df25713057086f0577f51cd2bb6c
pavel11988 <kossoua88@gmail.com>
2022-03-26 20:57:37 +0200

    fix-total

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,17 +8,17 @@ import { Container } from './App.styled';
 import Section from './Section/Section';
 import { useState, useEffect } from 'react';
 
+let total;
 export default function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
   const [positiveFeedback, setPositiveFeedback] = useState(0);
 
   useEffect(() => {
-    setTotal(good + neutral + bad);
+    total = good + neutral + bad;
     setPositiveFeedback(Math.round((good / total) * 100));
-  }, [good, neutral, bad, total]);
+  }, [good, neutral, bad]);
 
   const handleIncrement = name => {
     switch (name) {
